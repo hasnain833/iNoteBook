b56eaca42cfb6fe02357c572d5af17420838cbc7
Hasnain Aftab <hasnainaftab87@gmail.com>
2025-07-23 03:30:36 -0700

    first commit

---
+++ Backend/db.js
@@ -0,0 +1,10 @@
+const  mongoose = require("mongoose")
+const uri = "mongodb://localhost:27017/iNoteBook"
+
+const connectToMongo = () => {
+    mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => { console.log("Connected to MongoDB successfully") })
+        .catch(err => console.error("Could not connect to MongoDB", err));
+}
+
+module.exports = connectToMongo;
